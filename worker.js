6210e8fba2cf2a11202baeb9768934503b49bd3f
rayir03 <rayir03@gmail.com>
2024-12-21 13:43:22 -0600

    % fixed

---
+++ worker.js
@@ -0,0 +1,23 @@
+onmessage = async event => {
+    const { code , data , duration } = event.data;
+    let result;
+    try {
+        result = await eval(` (async () => {
+            let PERF__ops = 0;
+            let PERF__start = Date.now();
+            let PERF__end = Date.now() + ${duration};
+            ${data};
+            while(Date.now() < PERF__end) {
+                ${code};
+                PERF__ops ++;
+                }
+                return PERF__ops;
+        })()`) // IIFE ---> Inmediately Invoked Function Expression
+            
+    } catch (error) {
+        result = 0
+    }
+         
+    postMessage(result) 
+            
+}
